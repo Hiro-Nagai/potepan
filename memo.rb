f5a94c15df9cc758e0858491e9054d8a593f4409
[Hiro -Nagai] <[gm.komabun2003@gmail.com]>
2022-10-06 07:10:47 +0000

    memoowari commit

--- memo.rb
+++ memo.rb
@@ -11,9 +11,9 @@ if memo_type == 1
   imput_memo = STDIN.read
   memo = imput_memo.chomp
   
-  CSV.open("#{file_name}.csv" , "w") do |csv|
-  csv.puts ["#{memo}"]
- end
+    CSV.open("#{file_name}.csv" , "w") do |csv|
+      csv.puts ["#{memo}"]
+    end
  
 elsif memo_type == 2
   puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
@@ -23,10 +23,9 @@ elsif memo_type == 2
   imput_memo = STDIN.read
   memo = imput_memo.chomp
   
-  CSV.open("#{file_name}.csv","a") do |csv|
-  csv.puts ["#{memo}"]
-  
-end    
+    CSV.open("#{file_name}.csv","a") do |csv|
+      csv.puts ["#{memo}"]
+    end    
 
 else
   puts "1か2の数字を入力してください"
